78b9a7dacf98ad65211d2c828163b9bc76f5b370
bapan.rns <bapan.rns@gmail.com>
2023-06-12 12:31:31 +0530

    first commit2

--- app/controllers/productController.js
+++ app/controllers/productController.js
@@ -6,7 +6,7 @@ const path = require('path');
 const fs = require('fs');
 const uploadMiddleware = require("../helper/uploadMiddleware");
 const productImageModel = require("../models/productImageModel");
-
+ 
 
 
 
