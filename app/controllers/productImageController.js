5c74b1957b3f7ca37dd7ad7fba859e76fc1c7fdb
bapan.rns <bapan.rns@gmail.com>
2023-09-24 11:01:38 +0530

    product details page duplicate checking

--- app/controllers/productImageController.js
+++ app/controllers/productImageController.js
@@ -3,20 +3,32 @@ const { Op } = require('sequelize');
 
 const productImageModel = require("../models/productImageModel");
 const ProductModel = require("../models/productModel");
+const quantityModel = require("../models/quantityModel");
 
 async function getSameColorWiseItem(req, res){
     let imageArray = [];
 
     const products = await ProductModel.findAll({
         where: {
-            group_id: req.body.group_id
+            group_id: req.body.group_id,
+            active_status: 1
         },
         attributes: ['id', 'group_id'], 
-        include: {
-            model: productImageModel,
-            as: "Product_Image",
-            attributes: ['image_name']
-        }
+        include: [
+            {
+                model: productImageModel,
+                as: "Product_Image",
+                attributes: ['image_name']
+            },{
+                model: quantityModel,
+                as: "Quantity",
+                where: {
+                    no_of_product: {
+                        [Op.gt]: 0,
+                    }
+                }
+            }
+        ]
     }).then(productObj => {
         productObj.forEach((obj) => {
             let inner_hash = {
